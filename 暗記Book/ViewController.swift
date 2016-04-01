0a5e55a6e80d7eb83e17d71195a62c645d561025
T80 <t80@mizunoyuusuke-no-macbook-air-6.local>
2016-04-01 11:43:19 +0900

    Initial Commit

---
+++ 暗記Book/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  暗記Book
+//
+//  Created by T80 on 2016/04/01.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
